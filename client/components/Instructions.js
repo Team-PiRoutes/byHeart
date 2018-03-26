15e216546dbdba5337f39e6cc91b56f365f372d3
Vaida Armanaviciute <vaidaarman@gmail.com>
2018-03-26 11:56:43 -0500

    changed tab names, edited help popup

--- client/components/Instructions.js
+++ client/components/Instructions.js
@@ -5,16 +5,18 @@ import { List, Popup, Icon, Grid, Header } from 'semantic-ui-react'
 const Instructions = () => {
 
   const passageTips = [
-    'Start practising by slowly reading your passage a few times. Then increase the difficulty level',
+    'Start practising by slowly reading your passage a few times. When you are ready to move on, increase the difficulty level.',
     'To change the difficulty level - move the fader or use Arrow keys Left and Right.',
-    'If the box in the top right coner is not checked places of removed letters will be marked.'
+    'If the box in the top right coner is not checked places of removed letters will be marked.',
+    'Can not remember the word? Move the mouse above it for a hint.'
   ]
 
   const linesTips = [
     'Choose the difficulty level by moving the fader or using Arrow keys Left and Right. Click Start or press Enter key',
     'If the box in the top right coner is not checked places of removed letters will be marked.',
     'Click Next or press the Space key to move down the lines. Click Start Over or press Enter key to go back to the first line of the passage.',
-    'Check how long it took you to read all your lines. Save the time or/and start over by clicking on the button or pressing Enter key'
+    'Check how long it took you to read all your lines. Save the time or/and start over by clicking on the button or pressing Enter key.',
+    'Can not remember the word? Move the mouse above it for a hint.'
   ]
 
   return (
