91245f4fa2e9df794481e1141b16d92031f524d9
Kevin Kelly <kvinklly@gmail.com>
2018-03-28 12:03:08 -0500

    mobile-read-me

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -17,30 +17,30 @@ const Navbar = ({ handleClick, isLoggedIn, handleClickNew }) => (
           style={{ marginRight: '0.2em', padding: '0px' }}
         />
 </Menu.Item>*/}
-      <Menu.Item style={{ fontSize: '1.5em', padding: '0.7em' }}>
+      <Menu.Item style={{ fontSize: '1.5rem', padding: '0.7rem' }}>
         by<Link to="/">❤️</Link>
       </Menu.Item>
     </Menu.Menu>
     {isLoggedIn ?
-      <Menu.Menu position="right">
+      <Menu.Menu position="right" id="menu-right">
         {/* The navbar will show these links after you log in */}
         <Menu.Item>
-          <Button className="navButton" style={{ marginLeft: '0.5em' }} as={Link} to="/passages/new" href="#" onClick={handleClickNew}>New</Button>
-          <Button className="navButton" style={{ marginLeft: '0.5em' }} as={Link} to="/passages">Browse</Button>
-          <Button className="navButton" style={{ marginLeft: '0.5em' }} as={Link} to="/profile">Profile</Button>
-          <Button className="navButton" style={{ marginLeft: '0.5em' }} as={Link} to="/">Home</Button>
-          <Button className="navButton" style={{ marginLeft: '0.5em' }} as={Link} to="/" href="#" onClick={handleClick}>Logout</Button>
+          <Button className="navButton" style={{ marginLeft: '0.5rem' }} as={Link} to="/passages/new" href="#" onClick={handleClickNew}>New</Button>
+          <Button className="navButton" style={{ marginLeft: '0.5rem' }} as={Link} to="/passages">Browse</Button>
+          <Button className="navButton" style={{ marginLeft: '0.5rem' }} as={Link} to="/profile">Profile</Button>
+          <Button className="navButton" style={{ marginLeft: '0.5rem' }} as={Link} to="/">Home</Button>
+          <Button className="navButton" style={{ marginLeft: '0.5rem' }} as={Link} to="/" href="#" onClick={handleClick}>Logout</Button>
         </Menu.Item>
       </Menu.Menu>
       :
-      <Menu.Menu position="right">
+      <Menu.Menu position="right" id="menu-right">
         {/* The navbar will show these links before you log in */}
         <Menu.Item>
-          <Button className="navButton" style={{ marginLeft: '0.5em' }} as={Link} to="/passages/new">New</Button>
-          <Button className="navButton" style={{ marginLeft: '0.5em' }} as={Link} to="/passages">Browse</Button>
-          <Button className="navButton" style={{ marginLeft: '0.5em' }} as={Link} to="/">Home</Button>
-          <Button className="navButton" style={{ marginLeft: '0.5em' }} as={Link} to="/login">Login</Button>
-          <Button className="navButton" style={{ marginLeft: '0.5em' }} as={Link} to="/signup">Sign Up</Button>
+          <Button className="navButton" style={{ marginLeft: '0.5rem' }} as={Link} to="/passages/new">New</Button>
+          <Button className="navButton" style={{ marginLeft: '0.5rem' }} as={Link} to="/passages">Browse</Button>
+          <Button className="navButton" style={{ marginLeft: '0.5rem' }} as={Link} to="/">Home</Button>
+          <Button className="navButton" style={{ marginLeft: '0.5rem' }} as={Link} to="/login">Login</Button>
+          <Button className="navButton" style={{ marginLeft: '0.5rem' }} as={Link} to="/signup">Sign Up</Button>
         </Menu.Item>
       </Menu.Menu>
     }
